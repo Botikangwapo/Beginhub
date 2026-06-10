4b88b1849503e774634cc28875cff029eddf52eb
mjdev <jervinvillones6@gmail.com>
2026-06-10 17:43:11 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    cssMinify: true,
+    cssCodeSplit: false,
+  },
+})
